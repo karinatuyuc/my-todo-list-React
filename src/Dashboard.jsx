469283359aacd46f1ff6fd78e765a207a4277f92
Karina <karina8tuyuc@gmail.com>
2025-12-11 23:01:14 -0600

    Add header div with a title, input and a Magnifying icon. Left side

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,10 +1,25 @@
-
+import { MagnifyingGlassIcon, BellIcon, CalendarDaysIcon } from "@heroicons/react/24/outline"
 
 export default function Dashboard() {
+
     return (
         <>
             <header>
-                <h1 className="text-yellow-700">Dashboard</h1>
+                <h1>
+                    <span className="text-red-400 font-bold">Dash</span>
+                    <span className="text-black-950 font-bold">board</span>
+                </h1>
+                <div className="underline decoration-wavy decoration-red-400">
+                    <input
+                     className="border-amber-300 border"
+                        type="text"
+                        placeholder="Search your task here..."
+                    />
+                    < MagnifyingGlassIcon 
+                    className="w-6 h-6 text-red-400"
+                    />
+                </div>
+
             </header>
         </>
     )
