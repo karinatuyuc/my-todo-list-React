df1216c1f02c8301a23c3a751edbab74b004aaa5
Karina <karina8tuyuc@gmail.com>
2025-12-11 23:22:11 -0600

    Complete date logic and display in Dashboard header

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,15 +1,24 @@
 import { MagnifyingGlassIcon, BellIcon, CalendarDaysIcon } from "@heroicons/react/24/outline"
 
 export default function Dashboard() {
+    const currentDate = new Date();
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long', 
+        day: 'long'
+    }
+    const formattedDate = new Intl.DateTimeFormat('en-En', {weekday: 'long'}).format(currentDate);
 
     return (
         <>
+         <div>
             <header>
-                <h1>
+                <h1 className="text-3xl">
                     <span className="text-red-400 font-bold">Dash</span>
                     <span className="text-black-950 font-bold">board</span>
                 </h1>
-                <div className="underline decoration-wavy decoration-red-400">
+                <div className="decoration-red-400">
                     <input
                      className="border-amber-300 border"
                         type="text"
@@ -20,7 +29,22 @@ export default function Dashboard() {
                     />
                 </div>
 
+                <div className="w-32">
+                    <BellIcon 
+                     className="w-6 h-6 text-red-400"
+                    />
+                    < CalendarDaysIcon 
+                     className="w-6 h-6 text-red-400"
+                    />
+
+                    <span>{formattedDate}</span>
+                    <span
+                      className="text-blue-300"
+                    >{currentDate.toLocaleDateString()}</span>
+                </div>
+
             </header>
+         </div>
         </>
     )
 }
