a24215901052253b6a00bc382dbc086532a86edc
Karina <karina8tuyuc@gmail.com>
2025-12-16 23:25:49 -0600

    Add header layout with search input and ate display

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,35 +2,52 @@ import { MagnifyingGlassIcon, BellIcon, } from "@heroicons/react/24/outline";
 
 
 export default function Dashboard() {
+    const date = new Date(); // 
+    console.log(date) 
+    
+    const options = {
+        weekday: 'long',
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+
+    const dayName= date.toLocaleDateString('en-US', { weekday: 'long'}); // To show the day of the week
+    const dateInNumber = date.toLocaleDateString('en-US'); // To show the date in number 
+
     return (
         <>
          <div className="p-0.5 bg-yellow-300 h-screen w-full">
 
-            <header className="flex justify-between items-center p-1 bg-blue-200">
-                <div className= "font-bold">
+            <header className="flex justify-between items-center p-1 bg-red-100">
+
+                <div className= "font-bold text-xl">
                     <span className="text-black">Dash</span>
                     <span className="text-red-300">board</span>
                 </div>
 
-                <div className="bg-violet-300 relative w-64">
+                <div className="flex w-44 bg-white rounded-lg">
                     <input 
                       type="text"
-                      placeholder="Search your task here..."
-                      className="w-full rounded border pr-10 p-2 border-amber-950 "
+                      id="search"
+                      name="search"
+                      placeholder="Search your task..."
+                      className=" w-full h-8 p-2 text-sm rounded-lg focus:outline-none"
                     />
-                    <button className="absolute right-2 top-1">
+                    <button className="flex items-center justify-center">
                         <MagnifyingGlassIcon 
-                        className="w-5 h-5 absolute right-2 top-1 bg-red-300"/>
+                        className="w-7 h-7 p-2 bg-red-300 rounded-lg hover:bg-red-200"/>
                     </button>
                 </div>
 
-                <div className="bg-red-300">
-                    <span><BellIcon  className="w-5 h-5"/></span>
-                </div>
+                
+               { /* <div className="bg-red-300">
+                    <span><BellIcon  className="w-4 h-4"/></span>
+                </div> */}
 
-                <div className="bg-green-400 flex flex-col text-center">
-                    <span>Day</span>
-                    <span>Number</span>
+                <div className="flex flex-col">
+                    <span className="font-bold text-[10px]">{dayName}</span>
+                    <span className="font-bold text-[10px] text-cyan-400 ">{dateInNumber}</span>
                 </div>
             </header>
          </div>
