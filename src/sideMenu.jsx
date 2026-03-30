50bc797bd727a8f91051afd1e0db3b50c3dca5ca
Karina <karina8tuyuc@gmail.com>
2026-03-29 21:49:44 -0600

    interactive username

--- src/sideMenu.jsx
+++ src/sideMenu.jsx
@@ -62,6 +62,8 @@ export default function SideMenu() {
                 lg:text-2xl lg:flex lg:justify-center lg:items-center lg:text-center lg:mt-2">
 
                     <input
+                      type='text'
+                      value="John Doe"
                       placeholder="Write your name here"
                       className="w-52 ml-1 border-b-fuchsia-800 text-lg text-center"
                     />
@@ -101,57 +103,3 @@ export default function SideMenu() {
         </>
     )
 }
-
-/**
-      <div className="z-20 fixed w-full h-screen top-0 menu-side
-           lg:w-10
-           ">
-
-
-            <div className="absolute right-4 top-4 text-xl text-white ">
-                Close
-            </div>
-
-
-            <div className="bg-red-700 flex flex-col justify-center items-center lg:absolute lg:mt-20 lg:w-80 lg:flex lg:justify-center lg:items-center lg:bg-red-400">
-
-
-                <div className="rounded-full mt-4 border-white border-2 lg:mt-0">
-                    <img src={avatar} alt="avatar" className="w-20 h-20 rounded-full"/> 
-                </div>
-
-                <span className="text-2xl text-white font-semibold
-                lg:text-2xl lg:flex lg:justify-center lg:items-center lg:text-center lg:m-2">
-                    {userName}
-                </span>
-            </div>
-
-            <div className="bg-red-700 p-8 h-screen lg:bg-red-400 lg:w-80 ">
-                <ol className="space-y-12 lg:space-y-12 lg:mt-52 lg:hover:text-blue-900">
-                    <li className={listStyles}>
-                        <Squares2X2Icon className={iconStyles}/>
-                        Dashboard
-                    </li>
-                    <li className={listStyles}>
-                        <DocumentCheckIcon className={iconStyles}/>
-                        My Task
-                    </li>
-                    <li className={listStyles}>
-                        <ListBulletIcon className={iconStyles}/>
-                        Task Categories
-                    </li>
-                    <li className={listStyles}>
-                        <Cog6ToothIcon className={iconStyles}/>
-                        Settings
-                    </li>
-                    <li className={listStyles}>
-                        <ExclamationCircleIcon className={iconStyles}/>
-                        About
-                    </li>
-                    <li className={listStyles}>
-                        <QuestionMarkCircleIcon className={iconStyles}/>
-                        Help
-                    </li>
-                </ol>
-            </div>
-           </div> */
