c11f3271fd1f7bb0523f341afd054dc2c32f613b
Karina <karina8tuyuc@gmail.com>
2026-07-13 16:35:01 -0600

    feat: add delete task modal UI

--- src/optionModals/DeleteModal.jsx
+++ src/optionModals/DeleteModal.jsx
@@ -1,29 +1,32 @@
-export function DeleteTaskModal({ onDelete }) {
+export function DeleteTaskModal() {
 
-    /**
-     * iv className="fixed inset-50 bg-black bg-opacity-50 flex items-center justify-center">
-        <div className="bg-white rounded-lg shadow-xl w-96 p-6">
-          <h2 className="text-red-600 font-bold text-lg">Delete Task?</h2>
-          <p className="text-gray-700 mt-2">
-            This action cannot be undone. Are you sure you want to delete this
-            task?
-          </p>
-          <div className="flex justify-end gap-3 mt-4">
-            <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 rounded-md transition">
-              Cancel
-            </button>
-            <button className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md transition"
-            onClick={onDelete}>
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-     */
+    function cancelButton(){
+        console.log("Cancelar task");
+    }
+
+    function deleteButton(){
+        console.log("Delete task");
+    }
   return (
     <>
-     <div className="bg-white fixed">
-        <button className="w-60 h-60">Click to delete</button>
+     <div className="bg-amber-500/50 fixed inset-0 z-50 flex justify-center items-center">
+     <div className="bg-white rounded-lg shadow-xl w-96 p-6">
+        <h2 className="text-red-700 font-bold text-lg">Delete Task?</h2>
+        <p className="text-gray-700 mt-2">This action cannot be undone. Are you sure you want to delete this task?
+        </p>
+        <div className="flex justify-end gap-3 mt-4">
+
+            <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 rounded-md transition"
+            onClick={cancelButton}>
+                Cancel
+            </button>
+            <button className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md transition"
+            onClick={deleteButton}>
+                Delete
+            </button>
+        </div>
+     </div>
+        
      </div>
     </>
   );
